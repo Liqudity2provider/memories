9d1ca99059b87f4789af128c8a2ad723ed01590d
Alexander Hrechenko <alexander.grechenko@gmail.com>
2021-04-24 18:04:07 +0300

    add migrations to db
    1. init migrations
    2. tested migrations

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from app.config import Config
 
 
 db = SQLAlchemy()
 ma = Marshmallow()
+migrate = Migrate()
 
 
 bcrypt = Bcrypt()
@@ -28,6 +30,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
     from app.users.routes import users
     from app.post.routes import posts
     from app.main.routes import main
@@ -38,6 +41,3 @@ def create_app(config_class=Config):
     app.register_blueprint(errors)
 
     return app
-
-
-# db.create_all(app=create_app())
